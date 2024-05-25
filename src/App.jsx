74a21330f5f1055ae3c05d1169fa20eb359912bc
Wellington-Santos <wellingtonsantos.developer@gmail.com>
2024-05-25 19:28:50 -0300

    feat: Features has been added

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./Components/Home";
+import { Header } from "./Components/Header";
+import { UserStorage } from "./UserContext.jsx";
+import { PostPage } from "./Components/PagePost";
+import { About } from "./Components/About";
+import { Simulacao } from "./Components/Simulacao";
+import { Documentation } from "./Components/Documentation";
+import { Login } from "./Components/Login";
+import GoToTop from "./Components/GoToTop/index.jsx";
+import { AccountUser } from "./Components/AccountUser";
+import { FilterPage } from "./Components/FilterPage/index.jsx";
+import { Contact } from "./Components/Contact/index.jsx";
+import { Footer } from "./Components/Footer";
+import { InactiveProperties } from "./Components/InactiveProperties/index.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <UserStorage>
+        <Header />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/post/:id" element={<PostPage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/doc" element={<Documentation />} />
+            <Route path="/simulation" element={<Simulacao />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/accountuser/*" element={<AccountUser />} />
+            <Route path="/filtered/:id" element={<FilterPage />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/inactives" element={<InactiveProperties />} />
+          </Routes>
+        </div>
+        <GoToTop />
+        <Footer />
+      </UserStorage>
+    </BrowserRouter>
+  );
+}
+
+export default App;
